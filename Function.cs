ddcb1a0766d3d94f1a390c86c953f68e1843f050
Björn Sållarp <bjorn.sallarp@valtech.se>
2018-04-14 09:57:23 +0200

    Updated repro to more clearly expose the real problem. It's Azure runtime pulling in its own references instead of those referenced by the project. See: https://github.com/Azure/azure-functions-host/issues/1743

--- Function.cs
+++ Function.cs
@@ -1,49 +1,102 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.Azure.Management.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.AppService.Fluent.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Rest.Azure;
+using Microsoft.Rest.Serialization;
+using Newtonsoft.Json;
 
 namespace AzureFluentException
 {
     public static class Function
     {
-        private static string tenantId = "";
-        private static string clientId = "";
-        private static string clientSecret = "";
-
         [FunctionName("Function")]
-        public static async Task Run(
-            [TimerTrigger("0 */1 * * * *", RunOnStartup = true)]TimerInfo myTimer, 
-            TraceWriter log)
+        public static async Task Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)]TimerInfo myTimer, TraceWriter log)
         {
-            var credentials = GetCredentials();
-            var azure = Azure.Authenticate(credentials).WithDefaultSubscription();
+            // TransformationJsonConverter is the failing component. See issue where bug was solved:
+            // https://github.com/Azure/azure-sdk-for-net/issues/2501
+            // 
+            // But Azure runtime pulls in a different package than refenced by Microsoft.Azure.Management.Fluent
+            // and also 'Microsoft.Rest.ClientRuntime'-package explicitly referenced by this project. Appears to
+            // be an existing issue, see: https://github.com/Azure/azure-functions-host/issues/1743
+            var microsoftRestClientRuntime = typeof(TransformationJsonConverter).Assembly.Location;
+            log.Info($"Using dll: {microsoftRestClientRuntime}");
+            log.Info($"Version: {System.Diagnostics.FileVersionInfo.GetVersionInfo(microsoftRestClientRuntime).FileVersion}");
 
+            // Deserialize with latest TransformationJsonConverter from github
+            Deserialize(WorkingDeserializationSettings(), log);
+
+            // Deserialize the way it's done in fluent API
+            Deserialize(FluentPackaeDeserializationSettings(), log);
+        }
+
+        private static void Deserialize(JsonSerializerSettings settings, TraceWriter log)
+        {
             try
             {
-                // This throws error
-                var apps = await azure.WebApps.ListAsync();
+                var response = Resources.GetAzureResponse();
+                JsonConvert.DeserializeObject<Page<SiteInner>>(response, settings);
+                log.Info("Deserialization works!");
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                Console.WriteLine(e);
+                log.Error("Deserialization failed", ex);
                 throw;
             }
         }
 
-        private static AzureCredentials GetCredentials()
+        /// <summary>
+        /// This is code from WebSiteManagementClient in fluent package
+        /// </summary>
+        /// <returns></returns>
+        private static JsonSerializerSettings FluentPackaeDeserializationSettings()
         {
-            var env = AzureEnvironment.AzureGlobalCloud;
-            var principalLogin = new ServicePrincipalLoginInformation
+            var settings = new JsonSerializerSettings
             {
-                ClientId = clientId,
-                ClientSecret = clientSecret
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ReadOnlyJsonContractResolver(),
+                Converters = new List<JsonConverter>
+                {
+                    new Iso8601TimeSpanConverter()
+                }
+            };
+            settings.Converters.Add(new TransformationJsonConverter());
+            settings.Converters.Add(new CloudErrorJsonConverter());
+
+            return settings;
+        }
+
+        /// <summary>
+        /// This is code from WebSiteManagementClient in fluent package but instead uses
+        /// the TransformationJsonConverter from latest azure-sdk-for-net repo.
+        /// https://github.com/Azure/azure-sdk-for-net/blob/3f736b5af3851ab99bbaa98483ae537de0d48cfb/src/SdkCommon/ClientRuntime/ClientRuntime/Serialization/TransformationJsonConverter.cs
+        /// </summary>
+        /// <returns></returns>
+        private static JsonSerializerSettings WorkingDeserializationSettings()
+        {
+            var settings = new JsonSerializerSettings
+            {
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ReadOnlyJsonContractResolver(),
+                Converters = new List<JsonConverter>
+                {
+                    new Iso8601TimeSpanConverter()
+                }
             };
 
-            return new AzureCredentials(principalLogin, tenantId, env);
+            // Use code copied from https://github.com/Azure/azure-sdk-for-net/blob/3f736b5af3851ab99bbaa98483ae537de0d48cfb/src/SdkCommon/ClientRuntime/ClientRuntime/Serialization/TransformationJsonConverter.cs
+            settings.Converters.Add(new TransformationJsonConverter2());
+            settings.Converters.Add(new CloudErrorJsonConverter());
+
+            return settings;
         }
     }
 }
---
+++ Resources.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace AzureFluentException
+{
+    public class Resources
+    {
+        public static string GetAzureResponse()
+        {
+            const string resourceName = "AzureFluentException.AzureResponse.json";
+            var asm = typeof(Resources).Assembly;
+            using (var stream = asm.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
